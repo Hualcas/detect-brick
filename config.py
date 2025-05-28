4f1bd9e4d925cfdec0f4ea77a8cc313c1635cba0
Hualcas <ihualcasl@gmail.com>
2025-05-28 13:48:07 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-# config.py
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SECRET_KEY = os.environ.get("SECRET_KEY", "default_secret_key")
     MONGODB_SETTINGS = {
-        'db': 'databas',
-        'host': 'localhost',
-        'port': 27017
+        'host': os.environ.get("MONGODB_URI", "mongodb://localhost/ladrillos_db")
     }
