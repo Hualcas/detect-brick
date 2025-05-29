9dc5937073ab5ff75611e12a45bf3f17103e1f3c
Hualcas <ihualcasl@gmail.com>
2025-05-29 00:19:16 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-from app import create_app
 from download_model import descargar_modelo, descargar_yolov5 # type: ignore
-
 
 # Descargas automáticas
 descargar_modelo()
 descargar_yolov5()
 
+from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
